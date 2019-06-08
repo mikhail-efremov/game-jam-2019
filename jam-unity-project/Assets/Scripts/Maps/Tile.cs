885a90aa36593c1ef80972816b004ebad517208c
Oleksandr Rogovskyi <o.rogovskyi@plarium.com>
2019-06-08 11:10:49 +0300

    Added visual representing for tile.

--- jam-unity-project/Assets/Scripts/Maps/Tile.cs
+++ jam-unity-project/Assets/Scripts/Maps/Tile.cs
@@ -5,16 +5,19 @@ namespace UnityTemplateProjects.Maps
   public class Tile : MonoBehaviour
   {
     public bool IsBroken;
+    private MeshRenderer _renderer;
 
     public void Break()
     {
       IsBroken = true;
+      _renderer.material.color = Color.red;
       // animation?
     }
 
     public void Repair()
     {
       IsBroken = false;
+      _renderer.material.color = Color.green;
       // animation?
     }
   }
