1ea21c7bc622e65f632ff75dc8b29440d981b954
Oleksandr Rogovskyi <o.rogovskyi@plarium.com>
2019-06-09 14:38:17 +0300

    Added healthbar appear.

---
+++ jam-unity-project/Assets/Scripts/Appear.cs
@@ -0,0 +1,17 @@
+﻿using DG.Tweening;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Appear : MonoBehaviour
+{
+  public float Timeout;
+  public Image[] Targets;
+
+  void Start()
+  {
+    foreach (var target in Targets)
+    {
+      target.DOFade(1, Timeout);
+    }
+  }
+}
